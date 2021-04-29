e4151e53cfb0fd40565b4fee6a01d69927c4ef9f
Yevgeniy <zhe.tovtin@gmail.com>
2021-04-28 19:39:25 -0500

    changed back-end

--- src/redux/auth/auth-operations.js
+++ src/redux/auth/auth-operations.js
@@ -82,7 +82,9 @@ export default authOperations;
 import axios from 'axios';
 import authActions from './auth-actions';
 
-axios.defaults.baseURL = 'https://goit-phonebook-api.herokuapp.com';
+/* axios.defaults.baseURL = 'https://goit-phonebook-api.herokuapp.com'; */
+
+axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
 
 const token = {
   set(token) {
